9f777609fe2c50ce1da540fcc397453d1c0584be
Karol Ksionek <Karol.Ksionek@paramount.com>
2023-12-01 14:56:47 +0100

    Initial commit

--- src/main/kotlin/solution1.kt
+++ src/main/kotlin/solution1.kt
@@ -3,7 +3,7 @@ fun main() {
     var sum = 0
     try {
         file.forEachLine { line ->
-            sum += processLine(line)
+            sum += processLine2(line)
         }
     } catch (e: Exception) {
         println("An error occurred: ${e.message}")
@@ -22,6 +22,48 @@ fun processLine(line: String): Int {
                 lastDigit = firstDigit
             } else {
                 lastDigit = character.digitToInt()
+            }
+        }
+    }
+    return "$firstDigit$lastDigit".toInt()
+}
+
+val dictionary = mapOf(
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9,
+)
+
+fun processLine2(line: String): Int {
+    var firstDigit = -1
+    var lastDigit = -1
+    var buffer = StringBuffer()
+    for (character in line) {
+        if (character.isDigit()) {
+            buffer = StringBuffer()
+            if (firstDigit == -1) {
+                firstDigit = character.digitToInt()
+                lastDigit = firstDigit
+            } else {
+                lastDigit = character.digitToInt()
+            }
+        } else {
+            buffer.append(character)
+            val currentWord = buffer.toString()
+            val foundMatch = dictionary.entries.firstOrNull { (word, _) -> currentWord.endsWith(word) }?.value
+            if (foundMatch != null) {
+                if (firstDigit == -1) {
+                    firstDigit = foundMatch
+                    lastDigit = firstDigit
+                } else {
+                    lastDigit = foundMatch
+                }
             }
         }
     }
